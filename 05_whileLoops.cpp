5b3d858ec6c4d30daf45551f191672848c68d157
Siddharth <sid.sharma1704@gmail.com>
2022-10-27 19:36:52 +0530

    While Loops

---
+++ 05_whileLoops.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    while(n>0){
+        cout<<n<<endl;
+        cin>>n;
+    }
+
+    cout<<"Got a negative integer: "<<n<<endl<<"Exiting program...";
+
+    return 0;
+}
