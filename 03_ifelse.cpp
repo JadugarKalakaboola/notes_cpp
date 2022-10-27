990d8e6d4507138167ec9c19ad37be7f143f27e3
Siddharth <sid.sharma1704@gmail.com>
2022-10-27 12:09:08 +0530

    if/else conditionals

---
+++ 03_ifelse.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //Nested Loops
+
+    // int cash;
+    // cin>>cash;
+
+    // if(cash>10000){
+    //     if(cash > 20000){
+    //         cout<<"Trip to Ladakh"<<endl;
+    //     } else if (cash > 15000){
+    //         cout<<"Road trip on Golden Quadrilateral"<<endl;
+    //     } else if (cash > 10000){
+    //         cout<<"Trip to Rajasthan"<<endl;
+    //     }
+    // } else if (cash > 5000){
+    //     cout<<"Adventure in Rishikesh"<<endl;
+    // } else if (cash > 1000){
+    //     cout<<"Dinner with Family"<<endl;
+    // } else{
+    //     cout<<"Try saving some money T_T"<<endl;
+    // }
+
+    //To check the greatest number from 3 given numbers
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+    // if(a>b){
+    //     if(a>c){
+    //         cout<<a<<" is the greatest"<<endl;
+    //     }
+    //     else {
+    //         cout<<c<<" is the greatest"<<endl;
+    //     }
+    // } else {
+    //     if(b>c){
+    //         cout<<b<<" is the greatest"<<endl;
+    //     } else{
+    //         cout<<c<<" is the greatest"<<endl;
+    //     }
+    // }
+
+    //to check if a number is odd or even
+
+    int x;
+    cin>>x;
+
+    if (x%2==0){
+        cout<<"Even"<<endl;
+    } else {
+        cout<<"Odd"<<endl;
+    }
+
+    return 0;
+}
